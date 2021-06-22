224d9a3b7964f865642c01676f66719f1d614fcf
DavidChang <57895305+Davidchang24@users.noreply.github.com>
2021-06-22 12:51:33 +0200

    added homePage + some component name fixes

---
+++ src/components/Home/Home.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Home.sass";
+
+const Home = () => {
+  return (
+    <div>
+      <header class="header-home">
+        <div class="home">
+          <h1 class="title-home">Management</h1>
+          <a href="http://localhost:3002/add-dish" class="home-button pulsate">
+            Add Dishes
+          </a>
+          <a
+            href="http://localhost:3002/remove-dish"
+            class="home-button pulsate"
+          >
+            Dishes
+          </a>
+          <a
+            href="http://localhost:3002/add-ingredient"
+            class="home-button pulsate"
+          >
+            Ingredients
+          </a>
+          <a href="http://localhost:3002/tables" class="home-button pulsate">
+            Tables
+          </a>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Home;
